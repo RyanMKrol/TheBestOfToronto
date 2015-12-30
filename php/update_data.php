0baf3e07cb7c41906689f4634c250eaa87351acd
Ryan <ryankr@hotmail.co.uk>
2015-12-30 05:15:58 -0500

    Random entry being selected
    The backend integration is now done

--- php/update_data.php
+++ php/update_data.php
@@ -1,5 +1,16 @@
 <?php
+  //updates the data in the files by fetching everything
   exec ( 'sh ./../scripts/script.sh' );
-  echo file_get_contents('./../scripts/output/list.csv');
-?>
 
+  //the location of the data
+  $file = './../scripts/output/list.csv';
+
+  //converting the file to an array of strings
+  $lines = file($file);
+
+  //getting a random index into that file
+  $random_index = rand(0,count($lines));
+
+  //printing out the line
+  echo $lines[$random_index];
+?>
