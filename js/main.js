9af09405ce2df1c4e82a63ca566da0747074c89f
Ryan <ryankr@hotmail.co.uk>
2015-12-30 14:48:05 -0500

    Script to get the actual best activity finished

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+var base_link = 'http://www.blogto.com';
+
 $('button').click(function(){
   $.getJSON( "php/update_data.php", function( json ) {
+    var link = base_link + json.url;
     $('#activity_title').html(json.title);
+    $('#activity_link').attr('href', link);
    });
 });
 
---
+++ scripts/extract_number_one.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl http://www.blogto.com/toronto/the_best_guacamole_in_toronto/ > output/specific_data.txt
+python3 extract.py output/specific_data.txt | sed 's/.*ref=\"\(.*\)\"><img.*/\1/'
+
+
